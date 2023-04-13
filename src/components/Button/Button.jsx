047df496a20cacc2f4600b4c7b83a6792174a689
Enis Yavuz <enis.1739@gmail.com>
2023-04-13 20:13:12 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,6 +3,7 @@ import { View,Text,TouchableOpacity } from 'react-native'
 import styles from './Button.style'
 function Button({text,theme="primary",onPress}) {
   return (
+    //Button Section
     <TouchableOpacity style={styles[theme].container} onPress={onPress}>
         <Text style={styles[theme].text}>{text}</Text>
     </TouchableOpacity>
