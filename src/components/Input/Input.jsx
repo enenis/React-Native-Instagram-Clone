c9ea493653a588e5c5baefcf35afa2f6822eb07c
Enis Yavuz <enis.1739@gmail.com>
2023-04-13 20:13:35 +0200

    Update Input.jsx

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,6 +3,7 @@ import { View,TextInput, } from 'react-native'
 import styles from './Input.style'
 function Input({placeholder,value,onChange}) {
   return (
+    //Input section
     <View style={styles.container}>
         <TextInput placeholder={placeholder} value={value} onChangeText={onChange} />
     </View>
