de6847d2b1b7a9eaf1d5e3d3d0248deab7b22124
Enis Yavuz <enis.1739@gmail.com>
2023-06-23 01:48:38 +0300

    Update App-test.tsx

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,11 +1,7 @@
 /**
  * @format
  */
-
-
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
